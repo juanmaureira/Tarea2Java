83b4808b38f3ce3966cd7d797f60a2b5bd7ebbf4
elocker <elocker@elocker.cl>
2019-09-10 15:35:58 -0300

    Actualizacion

--- JavaApplication13/src/javaapplication13/Taxi.java
+++ JavaApplication13/src/javaapplication13/Taxi.java
@@ -9,6 +9,23 @@ package javaapplication13;
  *
  * @author juaneduardomaureirallanos
  */
-public class Taxi {
+public class Taxi extends Vehiculo{
+    private String numlicencia;
+    
+    public Taxi(){
+        super();
+        numlicencia=null;
+    }
+    
+    public Taxi(String patente, String modelo, Integer potencia, String numlicencia){
+        super(patente, modelo, potencia);
+        this.numlicencia=numlicencia;
+    }
+    
+    public String getNumlicencia(){
+        return numlicencia;
+    }
+    
+    
     
 }
